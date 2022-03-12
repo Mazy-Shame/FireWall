08bccdf730d23b9149499db1c5cffd5be9fd2155
Mazy_Shame <boss.masyanov@gmail.com>
2022-03-12 18:52:50 +0400

    add select window for WhiteHostsList

--- firewallproject/src/HostsAcList/HostAcList.jsx
+++ firewallproject/src/HostsAcList/HostAcList.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './HostAcList.module.css';
 import { useState } from 'react';
+import Select from 'react-select';
 
 function HostAcList() {
 
@@ -10,16 +11,72 @@ function HostAcList() {
 
     const [checked, setchecked] = useState()
 
+    const [optvalue, setoptvalue] = useState([])
+    const [optvalueun, setoptvalueun] = useState([])
+
+
+    useEffect(() => {
+        if (state.HostsList){
+
+            let options = []
+
+            state.HostsList.map( el => {
+                if (el.allowed == true){
+                    return(
+                        options.push({value: el.ip, label: el.ip})
+                    )
+                }
+            })
+
+            setoptvalue(options)
+
+            let optionsun = []
+
+            state.HostsList.map( el => {
+                if (el.allowed == false){
+                    return(
+                        optionsun.push({value: el.ip, label: el.ip})
+                    )
+                }
+            })
+
+            console.log(optionsun);
+            setoptvalueun(optionsun)
+            
+        }
+    }, [state.HostsList])
+
+
 
     const setAllowed = (allowed, ip) => {
-        if (allowed) {
-            dispatch({type: "SET-ALLOW", allowed: false, ip: ip})
-        }
-        else{
-            dispatch({type: "SET-ALLOW", allowed: true, ip: ip})
-        }
-        console.log(state);
+
     }
+
+    const changeOption = (value,action) => {
+
+
+        if ( action.action == 'remove-value'){
+
+           let optionsNow = optvalue
+           let optionsAfterdelete = optionsNow.filter(function(f) { return f !== action.removedValue })
+
+            setoptvalueun([...optvalueun, action.removedValue])
+            setoptvalue(optionsAfterdelete)
+            dispatch({type:'SET-ALLOW', allowed: false, ip: action.removedValue.value})
+        }
+        else if (action.action == 'select-option'){
+            let optionsNow = optvalueun
+         
+            let optionsAfterSelect = optionsNow.filter(function(f) { return f !== action.option })
+
+            setoptvalueun(optionsAfterSelect)
+            setoptvalue([...optvalue, action.option])
+            dispatch({type:'SET-ALLOW', allowed: true, ip: action.option.value})
+
+        }
+
+    }
+
 
     if (state.HostsList == null){
         return(
@@ -28,9 +85,13 @@ function HostAcList() {
     }
     else{
 
+
+        //console.log(state.HostsList);
+
         return (
             <div className={styles.ListContainer}>
-                {
+                <Select options={optvalueun} onChange={changeOption} isMulti={true} value={optvalue}/>
+                {/* {
                     state.HostsList.map( el => {
                         return (
                             <div >
@@ -40,7 +101,7 @@ function HostAcList() {
                             </div>
                         )
                     })
-                }
+                } */}
             </div>
           )
     }
